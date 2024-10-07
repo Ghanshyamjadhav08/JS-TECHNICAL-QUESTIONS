f307a1d91c6596d7b89da1d03d6c457b55c20be8
Shyam <jadhavshyam08@gmail.com>
2024-10-07 11:29:12 +0530

    s

--- js-interview/index.js
+++ js-interview/index.js
@@ -101,3 +101,23 @@ const mapResult =  arr.map((ar) => {
 });
 
 console.log(mapResult, forEachReuslt, arr)
+
+
+
+// map filter and reduce o/p based questions 
+// 1 : Return only name of students in Capital
+
+let students = [
+    {name: "priyanka", rollno: 44, marks:90},
+    {name:"shyam", rollno:43, marks: 89},
+    {name:"Kaushal", rollno: 16, marks:45},
+    {name:"Harshal", rollno:76, marks: 98}
+];
+
+let names =[];
+
+for(let i = 0; i < students.length; i++){
+    names.push(students[i].name.toUpperCase());
+}
+
+console.log(names); // [ 'PRIYANKA', 'SHYAM', 'KAUSHAL', 'HARSHAL' ] 
