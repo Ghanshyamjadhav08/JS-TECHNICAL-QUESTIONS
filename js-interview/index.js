8e234630050568078a9c6d4427f346ac40d4c015
Shyam <jadhavshyam08@gmail.com>
2024-10-07 10:34:09 +0530

    Update index.js
    filter polyfill

--- js-interview/index.js
+++ js-interview/index.js
@@ -29,6 +29,7 @@ console.log(sum); */
 
 //polyfill for map 
  
+/* 
 Array.prototype.myMap = function(cb){
     let temp = [];
     for (let i = 0; i < this.length; i++){
@@ -41,5 +42,23 @@ const multiplyThree = nums.myMap((num,i,arr) => {
     return num * 3;
 });
 console.log(multiplyThree);
+ */
 
+
+//polyfill for Filter 
+Array.prototype.filter = function(cb){
+    let temp = [];
+    for (let i = 0; i<this.length; i++){
+        if(cb(this[i], i, this)) temp.push(this[i]);
+    }
+    return temp;
+}
+
+const nums = [1,2,3,4];
+
+const moreThanTwo = nums.filter((num) => {
+    return num > 2;
+});
+
+console.log(moreThanTwo);
 
