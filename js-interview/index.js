5a29c54163260956f281f036c313185127d21909
Shyam <jadhavshyam08@gmail.com>
2024-10-07 10:26:55 +0530

    Create index.js
    polyfills for map

---
+++ js-interview/index.js
@@ -0,0 +1,45 @@
+//map filter reduce 
+
+/*const nums = [1,2,3,4,5];
+const multiplyThree = nums.map((num,i,arr) => {
+    return num* 3;
+});
+console.log(multiplyThree); [ 3, 6, 9, 12, 15 ]*/
+
+
+/* const nums = [1,2,3,4];
+const moreThanTwo = nums.filter((num)=> {
+    return num > 2;
+});
+
+console.log(moreThanTwo); */
+
+
+// reduce 
+
+/*const nums = [1,2,3,4];
+ 
+const sum = nums.reduce((acc, curr, i, arr)=>{
+    return acc + curr;
+},0)
+
+console.log(sum); */
+
+
+
+//polyfill for map 
+ 
+Array.prototype.myMap = function(cb){
+    let temp = [];
+    for (let i = 0; i < this.length; i++){
+        temp.push(cb(this[i], i, this));
+    }
+    return temp;
+};
+const nums = [1,2,3,4,5];
+const multiplyThree = nums.myMap((num,i,arr) => {
+    return num * 3;
+});
+console.log(multiplyThree);
+
+
